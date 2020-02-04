9daa3cc9aa085c572a3f6e9b983362be40ff3874
Aniel Shri <aniel.shri@alten.nl>
2020-02-04 13:03:05 +0100

    working on hall sensors

---
+++ Core/Inc/hall_sensors.h
@@ -0,0 +1,15 @@
+/*
+ * hall_sensors.h
+ *
+ *  Created on: Feb 4, 2020
+ *      Author: ashri
+ */
+
+#ifndef INC_HALL_SENSORS_H_
+#define INC_HALL_SENSORS_H_
+
+
+
+
+
+#endif /* INC_HALL_SENSORS_H_ */
---
+++ Core/Src/hall_sensors.c
@@ -0,0 +1,6 @@
+/*
+ * hall_sensors.c
+ *
+ *  Created on: Feb 4, 2020
+ *      Author: ashri
+ */
